670f0dd1d2c3ae5eecc755a2cbc47600d766aa07
mqaseem634 <47574255+mqaseem634@users.noreply.github.com>
2024-03-24 13:22:30 -0700

    added id

--- internet-gateway.tf
+++ internet-gateway.tf
@@ -23,7 +23,7 @@ resource "aws_internet_gateway" "igw-east" {
 }
 
 resource "aws_internet_gateway_attachment" "igw-attach-east" {
-  internet_gateway_id = aws_internet_gateway.igw-east
+  internet_gateway_id = aws_internet_gateway.igw-east.id
   vpc_id              = aws_vpc.vpc-east.id
   provider = aws.aws-east
 }
