f8ebf5e6a0b880a3491391c66eeb7304d136dd5b
mqaseem634 <47574255+mqaseem634@users.noreply.github.com>
2024-03-24 13:07:04 -0700

    added tags to vpc

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,15 @@
 resource "aws_vpc" "vpc-west" {
   cidr_block       = "10.5.0.0/16"
   provider = aws.aws-west
+  tags = {
+    Name = "tf-aws-west"
+  }
 }
 
 resource "aws_vpc" "vpc-east" {
   cidr_block       = "10.20.0.0/16"
   provider = aws.aws-east
+  tags = {
+    Name = "tf-aws-east"
+  }
 }
