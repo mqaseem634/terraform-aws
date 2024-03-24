4b53146d5f9b1f6c693be571e1ab285872090a56
mqaseem634 <47574255+mqaseem634@users.noreply.github.com>
2024-03-24 13:20:21 -0700

    Adding IGW attachements

--- internet-gateway.tf
+++ internet-gateway.tf
@@ -6,10 +6,22 @@ resource "aws_internet_gateway" "igw-west" {
   }
 }
 
+resource "aws_internet_gateway_attachment" "igw-attach-west" {
+  internet_gateway_id = aws_internet_gateway.igw-west.id
+  vpc_id              = aws_vpc.vpc-west.id
+}
+
+
+###################
 resource "aws_internet_gateway" "igw-east" {
   #vpc_id = aws_vpc.main.id
   provider = aws.aws-east
   tags = {
     Name = "tf-igw-east"
   }
+}
+
+resource "aws_internet_gateway_attachment" "igw-attach-east" {
+  internet_gateway_id = aws_internet_gateway.igw-east
+  vpc_id              = aws_vpc.vpc-east.id
 }
