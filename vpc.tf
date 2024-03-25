a31ca38e41f5677eec1aa70055a0426487f3d288
mqaseem634 <47574255+mqaseem634@users.noreply.github.com>
2024-03-25 16:16:11 -0700

    Updating VPC with dns and hostname support

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "vpc-west" {
   cidr_block       = "10.5.0.0/16"
   provider = aws.aws-west
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "tf-aws-west"
   }
@@ -9,6 +11,8 @@ resource "aws_vpc" "vpc-west" {
 resource "aws_vpc" "vpc-east" {
   cidr_block       = "10.20.0.0/16"
   provider = aws.aws-east
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "tf-aws-east"
   }
