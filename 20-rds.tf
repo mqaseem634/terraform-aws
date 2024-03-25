6a9f59c9e98f04106b29b33bfabbb5fdb30baca9
mqaseem634 <47574255+mqaseem634@users.noreply.github.com>
2024-03-25 15:45:58 -0700

    Adding RDS DB instances

--- 20-rds.tf
+++ 20-rds.tf
@@ -5,8 +5,38 @@ resource "aws_db_subnet_group" "subnet_group_west" {
 }
 
 
+resource "aws_db_instance" "rdsdb-west" {
+  provider = aws.aws-west
+  allocated_storage    = 10
+  db_name              = "terraform"
+  engine               = "mysql"
+  engine_version       = "8.0.35"
+  instance_class       = "db.t4g.micro"
+  username             = "allakos"
+  password             = "allakos123"
+  #parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.subnet_group_west.name
+}
+
 resource "aws_db_subnet_group" "subnet_group_east" {
   provider = aws.aws-east
   name       = "terraform"
   subnet_ids = [aws_subnet.subnet-east2a.id, aws_subnet.subnet-east2b.id]
+}
+
+
+
+resource "aws_db_instance" "rdsdb-east" {
+  provider = aws.aws-east
+  allocated_storage    = 10
+  db_name              = "terrafom"
+  engine               = "mysql"
+  engine_version       = "8.0.35"
+  instance_class       = "db.t4g.micro"
+  username             = "allakos"
+  password             = "allakos123"
+  #parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.subnet_group_east.name
 }
